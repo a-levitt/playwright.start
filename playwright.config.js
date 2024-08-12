1c4d36522d579d32d594e412b4ef35955501d8b3
a-levitt <artemis.levitt@gmail.com>
2024-08-12 22:27:32 +0200

    Fix for commented lines

--- playwright.config.js
+++ playwright.config.js
@@ -1,51 +1,51 @@
-/*
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 
-/!**
+/**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
- *!/
+ */
 // require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-/!**
+/**
  * @see https://playwright.dev/docs/test-configuration
- *!/
+ */
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
   expect: {
-    /!**
+    /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
-     *!/
+     */
     timeout: 5000
   },
-  /!* Run tests in files in parallel *!/
+  /* Run tests in files in parallel */
   fullyParallel: true,
-  /!* Fail the build on CI if you accidentally left test.only in the source code. *!/
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /!* Retry on CI only *!/
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /!* Opt out of parallel tests on CI. *!/
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /!* Reporter to use. See https://playwright.dev/docs/test-reporters *!/
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
-  /!* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. *!/
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /!* Base URL to use in actions like `await page.goto('/')`. *!/
+    /* Base URL to use in actions like `await page.goto('/')`. */
     //baseURL: 'http://127.0.0.1:3000',
 
-    /!* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer *!/
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
-  /!* Configure projects for major browsers *!/
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+/*
 
     {
       name: 'firefox',
@@ -56,8 +56,9 @@ module.exports = defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+*/
 
-    /!* Test against mobile viewports. *!/
+    /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
@@ -67,7 +68,7 @@ module.exports = defineConfig({
     //   use: { ...devices['iPhone 12'] },
     // },
 
-    /!* Test against branded browsers. *!/
+    /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
@@ -78,7 +79,7 @@ module.exports = defineConfig({
     // },
   ],
 
-  /!* Run your local dev server before starting the tests *!/
+  /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://127.0.0.1:3000',
@@ -86,4 +87,3 @@ module.exports = defineConfig({
   // },
 });
 
-*/
--- tests-examples/demo-todo-app.spec.js
+++ tests-examples/demo-todo-app.spec.js
@@ -1,3 +1,4 @@
+/*
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
@@ -418,32 +419,33 @@ async function createDefaultTodos(page) {
   }
 }
 
-/**
+/!**
  * @param {import('@playwright/test').Page} page
  * @param {number} expected
- */
+ *!/
  async function checkNumberOfTodosInLocalStorage(page, expected) {
   return await page.waitForFunction(e => {
     return JSON.parse(localStorage['react-todos']).length === e;
   }, expected);
 }
 
-/**
+/!**
  * @param {import('@playwright/test').Page} page
  * @param {number} expected
- */
+ *!/
  async function checkNumberOfCompletedTodosInLocalStorage(page, expected) {
   return await page.waitForFunction(e => {
     return JSON.parse(localStorage['react-todos']).filter(i => i.completed).length === e;
   }, expected);
 }
 
-/**
+/!**
  * @param {import('@playwright/test').Page} page
  * @param {string} title
- */
+ *!/
 async function checkTodosInLocalStorage(page, title) {
   return await page.waitForFunction(t => {
     return JSON.parse(localStorage['react-todos']).map(i => i.title).includes(t);
   }, title);
 }
+*/
