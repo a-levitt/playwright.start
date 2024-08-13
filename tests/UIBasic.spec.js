ff083964475a7b67f76bc0f8a13009507de23188
a-levitt <artemis.levitt@gmail.com>
2024-08-13 12:51:21 +0200

    Add assertions

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -1,7 +1,8 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 test("Default test", async ({page}) => {
     await page.goto("https://www.hipotekarnabanka.com/");
+    await expect(page).toHaveTitle("Hipotekarna Banka");
     //await page.pause();
 });
 
@@ -9,6 +10,7 @@ test("Test with browser context", async ({browser}) => {
     const context = await  browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-
+    const getTitle = await page.title();
+    expect(getTitle).toBe("LoginPage Practise | Rahul Shetty Academy");
     //await page.pause();
 });
