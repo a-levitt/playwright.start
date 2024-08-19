8eab40df85e2a60ee0defff8b229c8744ee4d753
a-levitt <artemis.levitt@gmail.com>
2024-08-19 14:39:35 +0200

    Get first product title after successful login

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -9,19 +9,28 @@ test.skip("Default test", async ({page}) => {
 test("Test with browser context", async ({browser}) => {
     const context = await  browser.newContext();
     const page = await context.newPage();
+
+    const fieldUserName = await page.locator("//input[@id='username']");
+    const fieldPassword = await page.locator("//input[@id='password']");
+    const buttonSignIn = await page.locator("//input[@id='signInBtn']");
+    const alertWrongUserData = await page.locator("//form[@id='login-form']/" +
+        "div[@class='alert alert-danger col-md-12']");
+    const productIphoneTitle = await page.locator("//div[@class='card-body']//a").first();
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const getTitle = await page.title();
     expect(getTitle).toBe("LoginPage Practise | Rahul Shetty Academy");
-    const fieldUserName = await page.locator("//input[@id='username']");
+
     await fieldUserName.fill("rahulshetty");
-    const fieldPassword = await page.locator("//input[@id='password']");
     await fieldPassword.fill("learning");
-    const buttonSignIn = await page.locator("//input[@id='signInBtn']");
     await buttonSignIn.click();
-    const alertWrongUserData = await page.locator("//form[@id='login-form']/" +
-                                                                    "div[@class='alert alert-danger col-md-12']");
     const alertText = await alertWrongUserData.textContent();
-    //await console.warn("1: " + alertText);
+    //await console.warn(alertText);
     await expect(alertText).toContain("Incorrect");
     //await page.pause();
+
+    await fieldUserName.clear();
+    await fieldUserName.fill("rahulshettyacademy")
+    await buttonSignIn.click();
+    await console.warn(await productIphoneTitle.textContent());
 });
